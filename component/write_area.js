8c08cbcab3689c6eae64b478ef11b1ca887aa422
Kinakopan <mtakagi@my.bcit.ca>
2023-02-07 17:35:43 -0800

    added posting function

--- component/write_area.js
+++ component/write_area.js
@@ -6,15 +6,13 @@ export default function WriteArea() {
   return (
   <div className={styles.background}>
     <div className={styles.paper}>
-      <input className={styles.title} 
+      <input className={styles.title}
       placeholder="Entry Title"
       ></input>
       <Weather></Weather>
       <input className={styles.type_area} type="text">
       </input>
     </div>
-
-    
   </div>
   )
 
